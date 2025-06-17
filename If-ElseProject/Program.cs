6038b957538b94e78a0b7cd4e1c44b01286beb67
selinozluk <selinozluk36@gmail.com>
2025-06-17 16:28:17 +0300

    add number evaluation with if-else and even/odd check

--- If-ElseProject/Program.cs
+++ If-ElseProject/Program.cs
@@ -6,35 +6,36 @@ namespace IfElseProject
     {
         static void Main(string[] args)
         {
-               // Kullanıcıdan sayı girişi
-                Console.WriteLine("Lütfen bir sayı giriniz:");
+            // Kullanıcıdan sayı girişi
+            Console.WriteLine("Lütfen bir sayı giriniz:");
 
-        // Kullanıcının girdiği değeri string olarak alarak integer değerine dönüştürür ve num değişkenine atar
-                int num = Convert.ToInt32(Console.ReadLine());
+            // Kullanıcının girdiği değeri string olarak alır, integer'a çevirir
+            int num = Convert.ToInt32(Console.ReadLine());
 
-                // Sayının 10'dan büyük olup olmadığı sorgusu
-                 if (num > 10)
-                {
-                    // Sayı > 10 ise burası çalışır
+            // Sayının 10'a göre durumu
+            if (num == 10)
+            {
+                Console.WriteLine("Girilen sayı değeri 10'a eşittir.");
+            }
+            else if (num < 10)
+            {
+           // Sayının 10'dan büyük olup olmadığı sorgusu
+                Console.WriteLine("Girilen sayı 10'dan küçüktür.");
+            }
+            else
+            {
                 Console.WriteLine("Girilen sayı 10'dan büyüktür.");
+            }
 
-                    // Sayı çift mi değil mi sorgusu
-                 if (num % 2 == 0)
-                    {
-                    // Hem büyük hem de çiftse bu blok çalışır
-                        Console.WriteLine("Girilen sayı çifttir.");
-                    }
-                    else
-                    {
-                   // Sayı hem büyük hem tekse çalışır
-                        Console.WriteLine("Girilen sayı tektir");
-                    }
-                }
-                else
-                {
-                  // Sayı 10'dan küçükse veya eşitse burası çalışır
-                    Console.WriteLine("Girilen sayı 10'dan küçük veya eşittir.");
-                    }
+            // Sayı çift mi değil mi sorgusu
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("Girilen sayı çifttir.");
+            }
+            else
+            {
+                Console.WriteLine("Girilen sayı tektir.");
             }
         }
-    } 
+    }
+}
